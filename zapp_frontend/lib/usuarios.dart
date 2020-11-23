9215ade64c33f19a3f0fa488bfb29be8b40ff377
angela64 <33316044+angela64@users.noreply.github.com>
2020-11-23 10:46:01 +0100

    update usuarios.dart
    Falta sustituir el string de ejemplo por el de la base de datos

--- zapp_frontend/lib/usuarios.dart
+++ zapp_frontend/lib/usuarios.dart
@@ -1,27 +1,96 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 
-class Usuarios extends StatelessWidget {
-  Usuarios();
-  TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
+
+class ListSearch extends StatefulWidget {
+  ListSearchState createState() => ListSearchState();
+}
+
+
+class ListSearchState extends State<ListSearch> {
+
+  TextEditingController _textController = TextEditingController();
+  // String lista
+  static List<String> mainDataList = [
+          "Apple",
+          "Apricot",
+          "Banana",
+          "Blackberry",
+          "Coconut",
+          "Date",
+          "Fig",
+          "Gooseberry",
+          "Grapes",
+          "Lemon",
+          "Litchi",
+          "Mango",
+          "Orange",
+          "Papaya",
+          "Peach",
+          "Pineapple",
+          "Pomegranate",
+          "Starfruit"
+        ];
+
+  // Copy Main List into New List.
+  List<String> newDataList = List.from(mainDataList);
+
+  onItemChanged(String value) {
+    setState(() {
+      newDataList = mainDataList
+          .where((string) => string.toLowerCase().contains(value.toLowerCase()))
+          .toList();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-            child: Padding(
-                padding: const EdgeInsets.all(36.0),
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      SizedBox(
-                          child: Center(
-                        child: Text("Pagina usuarios en obras",
-                            style: TextStyle(
-                                fontFamily: 'Montserrat',
-                                fontSize: 20.0,
-                                fontWeight: FontWeight.bold)),
-                      ))
-                    ]))));
+        resizeToAvoidBottomPadding: false,
+        body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: TextField(
+              controller: _textController,
+              decoration: InputDecoration(
+                hintText: 'Búsqueda...',
+              ),
+              onChanged: onItemChanged,
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              padding: EdgeInsets.all(12.0),
+              children: newDataList.map((data) {
+                return ListTile(
+                  title: Text(data),
+                  onTap: () => print(data),);
+              }).toList(),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
+
+class Usuarios extends StatelessWidget {
+  Usuarios();
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            resizeToAvoidBottomPadding: false,
+            appBar: AppBar(
+                title: Text('Búsqueda usuarios')
+            ),
+            body: Center(
+                child: ListSearch()
+            )
+        )
+    );
+  }
+
+}
+
