f5ac7a178c52124592630fe329f44308e89d0244
maxisuarez <maxisuarez@corrreo.ugr.es>
2020-12-03 12:05:07 +0100

    login facilitador hecho

--- zapp_frontend/lib/loginVoluntario.dart
+++ zapp_frontend/lib/loginVoluntario.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
 import 'vistasVoluntario.dart';
 import 'myTextFormField.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:io';
+import 'package:fluttertoast/fluttertoast.dart';
+
+const Map<String, String> header = {
+  'Content-type': 'application/json',
+  'Accept': 'application/json',
+};
 
 class LoginVoluntario extends StatefulWidget {
   LoginVoluntario({Key key, this.title}) : super(key: key);
@@ -19,50 +28,7 @@ class _LoginVoluntario extends State<LoginVoluntario> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-
-    /*final emailField = TextField(
-      obscureText: false,
-      style: style,
-      decoration: InputDecoration(
-          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-          hintText: "Usuario",
-          border:
-              OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
-    );
-
-    final passwordField = TextField(
-      obscureText: true,
-      style: style,
-      decoration: InputDecoration(
-          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-          hintText: "Contraseña",
-          border:
-              OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
-    );
-
-    final loginButon = Material(
-      elevation: 5.0,
-      borderRadius: BorderRadius.circular(30.0),
-      color: Color(0xff01A0C7),
-      child: MaterialButton(
-        minWidth: MediaQuery.of(context).size.width,
-        padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-        onPressed: () {
-          Navigator.push(context,
-              MaterialPageRoute(builder: (context) => VistasVoluntario()));
-        },
-        child: Text("Entrar",
-            textAlign: TextAlign.center,
-            style: style.copyWith(
-                color: Colors.white, fontWeight: FontWeight.bold)),
-      ),
-    );*/
+    const url = 'http://zapp.pythonanywhere.com/loginFacilitador/';
     final login = Material(
       elevation: 5.0,
       borderRadius: BorderRadius.circular(30.0),
@@ -74,15 +40,39 @@ class _LoginVoluntario extends State<LoginVoluntario> {
           if (_formKey.currentState.validate()) {
             _formKey.currentState.save();
 
-            //const url = 'https://pythoneverywhere/alta_socio';
-            //await http.post(url,
-            //    body: jsonEncode(<String, String>{
-            //      'nombre': model.nombre,
-            //    }));
             print(codigo);
             print(contrasena);
-            Navigator.push(context,
-                MaterialPageRoute(builder: (context) => VistasVoluntario()));
+
+            http.Response response = await http.post(
+              url,
+              body: jsonEncode(<String, String>{
+                'username': codigo,
+                'password': contrasena,
+              }),
+              headers: {
+                HttpHeaders.acceptHeader: 'application/json',
+                HttpHeaders.contentTypeHeader: 'application/json',
+              },
+            );
+
+            final jsonResponse = jsonDecode(response.body);
+
+            if (jsonResponse.containsKey("error")) {
+              print("Usuario no registrado");
+              Fluttertoast.showToast(
+                  msg: "Codigo o contraseña incorrecto",
+                  toastLength: Toast.LENGTH_SHORT,
+                  gravity: ToastGravity.TOP,
+                  timeInSecForIosWeb: 1,
+                  backgroundColor: Colors.red,
+                  textColor: Colors.white,
+                  fontSize: 16.0);
+            }
+            if (jsonResponse.containsKey("success")) {
+              print("Usuario de logea");
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => VistasVoluntario()));
+            }
           }
         },
         child: Text("Iniciar sesión",
@@ -156,41 +146,3 @@ class _LoginVoluntario extends State<LoginVoluntario> {
                             ]))))));
   }
 }
-/*
-        body: Center(
-          child: Container(
-            color: Colors.white,
-            child: Padding(
-              padding: const EdgeInsets.all(36.0),
-              child: SingleChildScrollView(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    SizedBox(
-                      height: 155.0,
-                      child: Image.asset(
-                        "assets/logo.png",
-                        fit: BoxFit.contain,
-                      ),
-                    ),
-                    SizedBox(height: 45.0),
-                    emailField,
-                    SizedBox(height: 25.0),
-                    passwordField,
-                    SizedBox(
-                      height: 35.0,
-                    ),
-                    loginButon,
-                    SizedBox(
-                      height: 15.0,
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-        ));
-  }
-}
-*/
